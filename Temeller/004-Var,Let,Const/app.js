2670e0f1cdbb99e7867af47e98767fa55f49f4e7
Rıza Karabınar <rizakarabinar@gmail.com>
2018-12-02 19:03:28 +0300

    klasor isimlerine onceliklendirme eklendi

---
+++ Temeller/004-Var,Let,Const/app.js
@@ -0,0 +1,19 @@
+//Var ile degisken tanimlamak
+
+var a;
+console.log(a);
+console.log(typeof a);
+a = 20;
+console.log(a);
+console.log(typeof a);
+
+var a = "riza";
+console.log(a);
+console.log(typeof a);
+
+
+// Let ile degisken tanimlama
+
+let a = "riza";
+
+// let a = "karabinar"; //Tekrar tanimlamada hata verecektir.
