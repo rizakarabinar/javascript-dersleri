c679ecf5e49f7a7f051ddde65f456a132dc6224a
Rıza Karabınar <rizakarabinar@gmail.com>
2018-12-08 19:15:04 +0300

    dom manipulasyon giris yapildi

---
+++ Temeller/Javascript-ile-DOM-Manipulasyonu/017-Javascript-ile-Dom-maniplasyonu-giris/app.js
@@ -0,0 +1,5 @@
+//index hmtl de olusturdugumuz todo list uzerinden gidecegiz
+//Temel seviyede html ve css gerekliligi var
+
+//temel tag lar id ve class nedir, html, body, header, div vs gibi tagler ne ise yarar nasıl kullanilir gibi.
+// DOM ne demektir. Document Object Model
